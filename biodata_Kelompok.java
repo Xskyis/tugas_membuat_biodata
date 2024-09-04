c30ffa12d2ef323bbea7b25a8796615b3c54dee2
Qruzeeta Prasna <qruzeetaprasna15@gmail.com>
2024-09-04 16:09:04 +0700

    biodata zeta

--- biodata_Kelompok.java
+++ biodata_Kelompok.java
@@ -5,6 +5,14 @@ public class biodata_Kelompok {
         System.out.println("---------------------------------------------------------");
         System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
         System.out.println("---------------------------------------------------------");
+        System.out.println( "Nama\t: Qruzeeta Prasna");
+        System.out.println("NIM\t: 244107020084");
+        System.out.println("Kelas\t: 1A");
+        System.out.println("No.Absen: 22");
+        System.out.println("Prodi\t: Teknik Informatika");
+        System.out.println("Alamat\t: Jl. Natai Arahan no. 14");
+        System.out.println("No Hp\t: 085849423754");
+        System.out.println("Hobi\t: Makan");
     }
     
 }
