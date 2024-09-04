974e38815a9d9b42f85355f332b45d840b507aee
fazelazel <fazelpriyono@gmail.com>
2024-09-04 16:04:54 +0700

    Ini biodata Fazel

--- biodata_Kelompok.java
+++ biodata_Kelompok.java
@@ -5,6 +5,14 @@ public class biodata_Kelompok {
         System.out.println("---------------------------------------------------------");
         System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
         System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Fazel Priyono");
+        System.out.println("NIM     : 244107020033");
+        System.out.println("Kelas   : TI 1A");
+        System.out.println("No Absen: 12");
+        System.out.println("Prodi   : D IV TEKNIK INFORMATIKA");
+        System.out.println("Alamat  : Jalan Kembang Kertas No 27");
+        System.out.println("No HP   : 081332425388");
+        System.out.println("Hobi    : Badminton");
     }
     
 }
